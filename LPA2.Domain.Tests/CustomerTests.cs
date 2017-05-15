b2309936bea00492d96c5727b623066bd75fac25
Fabio Rezende <fabio.emailteste@gmail.com>
2017-05-14 23:27:09 -0300

    Criado teste de unidade

---
+++ LPA2.Domain.Tests/CustomerTests.cs
@@ -0,0 +1,35 @@
+﻿using LPA2.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LPA2.Domain.Tests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        private readonly User _user = new User("fabiorezende", "senha");
+
+        [TestMethod]
+        [TestCategory("Customer - New Customer")]
+        public void GivenAInvalidFirstNameShouldReturnANotification()
+        {
+            var customer = new Customer("", "Rezende", "fabio.rezende@gmail.com", _user);
+            Assert.IsFalse(customer.IsValid());
+        }
+
+        [TestMethod]
+        [TestCategory("Customer - New Customer")]
+        public void GivenAInvalidLastNameShouldReturnANotification()
+        {
+            var customer = new Customer("Fabio", "", "fabio.rezende@gmail.com", _user);
+            Assert.IsFalse(customer.IsValid());
+        }
+
+        [TestMethod]
+        [TestCategory("Customer - New Customer")]
+        public void GivenAInvalidEmailShouldReturnANotification()
+        {
+            var customer = new Customer("Fabio", "Rezende", "f", _user);
+            Assert.IsFalse(customer.IsValid());
+        }
+    }
+}
---
+++ LPA2.Domain.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LPA2.Domain.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LPA2.Domain.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7395d5d-c5d1-4123-919a-980a408065e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
